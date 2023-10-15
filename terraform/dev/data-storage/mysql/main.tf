fd39da716623ac1f566a809371c4202ee660d231
cybotixadmin <135337925+cybotixadmin@users.noreply.github.com>
2023-10-15 14:29:09 +0200

    change DB network

--- terraform/dev/data-storage/mysql/main.tf
+++ terraform/dev/data-storage/mysql/main.tf
@@ -10,6 +10,8 @@ provider "aws" {
 # VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
+ enable_dns_support = true
+  enable_dns_hostnames = true
   tags = {
     Name = "example-vpc"
   }
